2b6331668ab618f9071d0907def632ff6b14903b
lpicado <lpicado58@gmail.com>
2019-09-23 19:14:30 -0600

    Add project files.

---
+++ xxxx.bl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xxxx.bl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
